4cd9f731f6b64a874499002ff91c67e34835bcd8
Jesper Hodge <jhodge@outlook.de>
2024-03-16 18:04:32 -0400

    day2 bsl

--- src/day2/BinarySearchList.ts
+++ src/day2/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+  let start = 0;
+  let end = haystack.length - 1;
+  while (end >= start) {
+    if (end === start) {
+      return haystack[end] === needle;
+    }
+    const middle = start + Math.floor((end - start) / 2)
+    const guess = haystack[middle];
+    console.log('state: ', { start, end, middle, guess, needle, haystack })
+    if (guess === needle) {
+      return true;
+    }
+    if (guess < needle) {
+      start = middle + 1;
+    }
+    if (guess > needle) {
+      end = middle - 1;
+    }
+  }
+  return false;
 }
